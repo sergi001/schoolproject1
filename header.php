3152807a705a59dda682e29123549fdcec71a6fa
Sergi Ambrose <ambrosemusyoka47@gmail.com>
2023-01-10 16:22:39 +0300

    Added the navigation

---
+++ header.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tiffany Drug Store</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body> 
+<nav class="header">
+    <div class="navbar">
+            <div class="logo"><a href="#">TIFF</a></div>
+            <ul class="navs">
+                <li><a href="">Home</a></li>
+                <li><a href="" >About</a></li>
+                <li><a href="">Laptops</a></li>
+                <li><a href="">Mobile Phones</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">logout</a></li> 
+            </ul>
+    </div>
+</nav>
+    </body>
+</html>
